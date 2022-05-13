3b9294515ae5c2e5b28b2156a7b8e57155513f05
haocheng.zy <haocheng.zy@linux.alibaba.com>
2022-05-13 23:38:52 +0800

    Fix cts build error
    Change-Id: I0f0907d30a9d9eb938afe4d95146c2d6156608e3

--- linux/seccomp_bpf.c
+++ linux/seccomp_bpf.c
@@ -1243,7 +1243,7 @@ TEST_F(TRACE_poke, getpid_runs_normally)
 # define SYSCALL_NUM   gprs[2]
 # define SYSCALL_RET   gprs[2]
 #elif defined(__riscv)
-# define ARCH_REGS	struct user_pt_regs
+# define ARCH_REGS	struct user_regs_struct
 # define SYSCALL_NUM	a7
 # define SYSCALL_RET	a0
 #else
